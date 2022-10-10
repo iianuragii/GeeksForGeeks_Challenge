dc8fed34e6034f0d12aeacab26b54aa1c8cbc332
Anurag Dutta <imanurag18@gmail.com>
2022-10-10 13:55:57 +0530

    Update Min_Sumof_Abs_Difference.java

--- Min_Sumof_Abs_Difference.java
+++ Min_Sumof_Abs_Difference.java
@@ -1,4 +1,32 @@
-//User function Template for Java
+import java.io.*;
+import java.util.*;import java.util.collections;
+
+class Anurag
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int p = sc.nextInt();
+        int i;
+        while(p>0)
+        {
+            int N = sc.nextInt();
+            int A[] = new int[N];
+            int B[] = new int[N];
+
+             for(i = 0; i < N; i++)
+                A[i] = sc.nextInt();
+
+             for(i = 0; i < N; i++)
+                B[i] = sc.nextInt();
+        
+            Solution ob = new Solution();
+            long ans = ob.findMinSum(A,B,N);
+            System.out.println(ans);
+            t--;
+        }
+    }
+}
 
 class Solution { 
     long findMinSum(int[] A,int[] B,int N) 
@@ -6,9 +34,9 @@ class Solution {
                Arrays.sort(A);
         Arrays.sort(B);
         long answer=0;
-        for(int i=0;i<N;i++)
+        for(int i = 0 ; i < N; i++)
         {
-            answer+=Math.abs(A[i]-B[i]);
+            answer+= Math.abs(A[i]-B[i]);
         }
         return answer;
     }
